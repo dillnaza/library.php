5f8861de4587430cb16b456d253b480c9613259e
dillnaza <dilnazbaidakhanova@gmail.com>
2022-02-17 11:48:22 +0300

    in

--- index.php
+++ index.php
@@ -91,8 +91,8 @@ FROM category INNER JOIN book ON category.count = book.category
 WHERE ((category.count)=$cat);");
                 while ($result = mysqli_fetch_array($sql))
                     echo "<div content id='block'>{$result['name']}
-<img id='change' class='images' src='/css/noun-edit-1644623.png' onclick='changeForm()' alt='Изменить'>
-<img id='dell' class='images' src='/css/noun-delete-4602521.png' onclick='openDeleteForm()' alt='Удалить'>
+<img id='dell' style='float: right;' class='images' src='/css/noun-delete-4602521.png' onclick='openDeleteForm()' alt='Удалить'>
+<img id='change' style='float: right;' class='images' src='/css/noun-edit-1644623.png' onclick='changeForm()' alt='Изменить'>
 </div> </br>";
                 ?>
             </h3>
