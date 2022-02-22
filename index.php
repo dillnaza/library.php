4e1b8cf9b9417e2a41bf870ff4c67c6c382e9b15
dillnaza <dilnazbaidakhanova@gmail.com>
2022-02-22 16:46:05 +0300

    in

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 <head>
     <title>Библиотека</title>
     <meta charset="UTF-8">
-    <link rel="stylesheet" id="theme-style"  href="plugins/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" id="theme-style" href="plugins/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" id="theme-style" href="css/style.css">
 </head>
 
-<body  data-spy="scroll">
+<body data-spy="scroll">
 <header id="header" class="header">
     <div class="container">
         <nav role="navigation">
@@ -23,28 +23,28 @@
 <form id="promo" class="promo section offset-header" method="get">
     <div class="container text-center">
         <div class="btn" id="butt">
-        <buttton class="btn btn-cta-secondary" value=1 name="cat[]">1-категория</buttton>
-        <buttton class="btn btn-cta-primary" value="2" name="cat[]">2-категория</buttton>
-        <buttton class="btn btn-cta-secondary" value="3" name="cat[]">3-категория</buttton>
-        <buttton class="btn btn-cta-primary" value="4" name="cat[]">4-категория</buttton>
-        <buttton class="btn btn-cta-secondary"  value="5" name="cat[]">5-категория</buttton>
-    </div>
+            <button class="btn btn-cta-secondary" value=1 name="cat[]">1-категория</button>
+            <button class="btn btn-cta-primary" value="2" name="cat[]">2-категория</button>
+            <button class="btn btn-cta-secondary" value="3" name="cat[]">3-категория</button>
+            <button class="btn btn-cta-primary" value="4" name="cat[]">4-категория</button>
+            <button class="btn btn-cta-secondary" value="5" name="cat[]">5-категория</button>
+        </div>
     </div>
 </form>
 
-<?php include 'connect.php'?>
+<?php include 'connect.php' ?>
 
 <form id="catForm" align="center">
     <div>
-        <h2>Список книг <?php echo $cat=2?> категории:</h2>
+        <h2>Список книг <?php echo $cat = 1 ?> категории:</h2>
         <div>
             <div style="padding-left: 65%">
-            <button id="add" class="btn btn-cta-secondary" onclick="openAddForm()">Добавить новую книгу</button>
+                <button id="add" class="btn btn-cta-secondary" onclick="openAddForm()">Добавить новую книгу</button>
             </div>
-            <h4 style="color: #122b40; padding-right: 65%" >
+            <h4 style="color: #122b40; padding-right: 65%">
                 <img src="/css/noun-sort-1590833.png" class="images" onclick="sort()" alt="Сортироваит">
                 Количество книг:
-                <?php include 'category.php'?>
+                <?php include 'category.php' ?>
             </h4>
             <h3>
                 <?php include 'bookname.php' ?>
@@ -56,9 +56,10 @@
 
 <script>
     function openAddForm() {
-    event.preventDefault();
-    document.getElementById('addForm').style.display = "block";
+        event.preventDefault();
+        document.getElementById('addForm').style.display = "block";
     }
+
     function changeForm() {
         event.preventDefault();
         document.getElementById('changeForm').style.display = "block";
@@ -79,16 +80,18 @@
 
 </script>
 
-<form align="center" id="addForm" style="display: none"  class="hystmodaladd" method="post">
+<form align="center" id="addForm" style="display: none" class="hystmodaladd" method="post">
     <h3 class="fw-bold mb-0">Добавить данные</h3><br>
     <div>
         <label for="bookInput"> Называние книги:</label>
         <input type="text" name="bookInput" id="bookInput">
-    </div><br>
+    </div>
+    <br>
     <div>
         <label for="catInput">Категория книги:</label>
         <input type="text" name="catInput" id="catInput">
-    </div><br>
+    </div>
+    <br>
     <button type="submit" class="btn btn-cta-secondary" onclick="reAddForm()">Добавить</button>
     <button class="btn btn-cta-secondary" onclick="closeAddForm()">Закрыть</button>
 </form>
@@ -110,18 +113,20 @@
     }
 </script>
 
-<form align="center" id="changeForm" style="display: none;"  class="hystmodalchan" method="post">
+<form align="center" id="changeForm" style="display: none;" class="hystmodalchan" method="post">
     <h3>Изменение</h3>
     <div>
         <label for="book1Input"> Называние книги:</label>
         <input type="text" name="book1Input" id="book1Input">
-    </div><br>
+    </div>
+    <br>
     <div>
         <label for="cat1Input">Категория книги: </label>
         <input type="text" name="сat1Input" id="cat1Input">
-    </div><br>
+    </div>
+    <br>
     <button type="submit " class="btn btn-cta-secondary" onclick="reChangeForm()">Изменить</button>
-    <button  class="btn btn-cta-secondary" onclick="closeChangeForm()">Закрыть</button>
+    <button class="btn btn-cta-secondary" onclick="closeChangeForm()">Закрыть</button>
 </form>
 
 <script>
@@ -141,12 +146,16 @@
     }
 </script>
 
-<form align="center" id="deleteForm" style="display: none;"  class="hystmodaldel">
+<form align="center" id="deleteForm" style="display: none;" class="hystmodaldel">
     <h3>Удаление</h3>
     <p>Вы уверены, что хотите удалить?</p>
     <div>
-        <button type="submit " class="btn btn-lg btn-link fs-6 text-decoration-none col-6 m-0 rounded-0 border-right" onclick="deleteForm()">Удалить</button>
-        <button  class="btn btn-lg btn-link fs-6 text-decoration-none col-6 m-0 rounded-0" onclick="cancelForm()">Отмена</button>
+        <button type="submit " class="btn btn-lg btn-link fs-6 text-decoration-none col-6 m-0 rounded-0 border-right"
+                onclick="deleteForm()">Удалить
+        </button>
+        <button class="btn btn-lg btn-link fs-6 text-decoration-none col-6 m-0 rounded-0" onclick="cancelForm()">
+            Отмена
+        </button>
     </div>
 </form>
 
